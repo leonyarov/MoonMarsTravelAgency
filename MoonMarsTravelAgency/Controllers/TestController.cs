62531d612c5a870f74b4157ed3ff8d43a34f21dd
leon-yarov <leon44ml@gmail.com>
2022-12-05 18:47:30 +0200

    Updated databases

--- MoonMarsTravelAgency/Controllers/TestController.cs
+++ MoonMarsTravelAgency/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoonMarsTravelAgency.Models;
 
 namespace MoonMarsTravelAgency.Controllers
 {
@@ -11,6 +12,7 @@ namespace MoonMarsTravelAgency.Controllers
         // GET: Test
         public ActionResult Index()
         {
+            
             ViewBag.time = DateTime.Now;
             return View();
         }
---
+++ MoonMarsTravelAgency/Models/Moons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+
+namespace MoonMarsTravelAgency.Models
+{
+    public class Moons 
+    {
+        //public List<string> getMoons()
+        //{
+        //    List<string> crater = new List<string>();
+        //    List<string> locationX = new List<string>();
+        //    List<string> locationY = new List<string>();
+        //    List<string> Radius = new List<string>();
+        //    using (var reader = new StreamReader(Server.MapPath("~/App_Data/filename"))
+        //    {
+        //        while (!reader.EndOfStream)
+        //        {
+        //            var line = reader.ReadLine();
+        //            var values = line.Split(',');
+
+        //            crater.Add(values[0]);
+        //            locationX.Add(values[1]);
+        //            locationY.Add(values[2]);
+        //            Radius.Add(values[3]);
+        //        }
+        //    }
+        //    return crater;
+        //}
+
+        public void validateMoons()
+        {
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer
+(Configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
