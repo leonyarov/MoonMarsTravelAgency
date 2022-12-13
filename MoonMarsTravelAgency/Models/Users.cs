de79c912c1fe9a617dcd34f506a6da452bc427f5
Dori Fourer <dori.fourer654@gmail.com>
2022-12-13 23:08:50 +0200

    added required stetments

--- MoonMarsTravelAgency/Models/Users.cs
+++ MoonMarsTravelAgency/Models/Users.cs
@@ -11,21 +11,25 @@ namespace MoonMarsTravelAgency.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
 
         [Key]
-        [Required(ErrorMessage = "ID is required")]
-        [RegularExpression("^[0-9]{4}$")]
+        [Required(ErrorMessage = "ID number is required")]
+        [RegularExpression("^[0-9]{4}$", ErrorMessage = "ID number must contain 4 digits")]
         public int ID { get; set; }
 
-        [StringLength(50)]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must have at least 2 character. The maximum length is 50 character")]
         public string Name { get; set; }
-
-        [StringLength(50)]
+        
         [Column("Last Name")]
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must have at least 2 character. The maximum length is 50 character")]
         public string Last_Name { get; set; }
 
-        [StringLength(50)]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must have at least 2 character. The maximum length is 50 character")]
         public string Username { get; set; }
 
-        [StringLength(50)]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Password must have at least 2 character. The maximum length is 50 character")]
         public string Password { get; set; }
     }
 }
